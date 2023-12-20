ccdaddb88c4e4418e62d519a44f4c070fc5b04f2
FuriousFlash <adityakaushik38@gmail.com>
2023-12-20 21:12:30 +0400

    Updating jest config

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,11 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   testEnvironment: "jest-environment-jsdom",
   testMatch: ["<rootDir>/tests/**/*.test.js"],
+  moduleNameMapper: {
+    "^@components/(.*)$": "<rootDir>/src/components/$1",
+    "^@models/(.*)$": "<rootDir>/src/models/$1",
+    "^@pages/(.*)$": "<rootDir>/src/pages/$1",
+    "^@styles/(.*)$": "<rootDir>/src/styles/$1",
+    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+  },
 };

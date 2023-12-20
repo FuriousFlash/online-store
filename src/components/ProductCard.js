0b82db46154db453ababcdbeaf53ea78e7394bab
FuriousFlash <adityakaushik38@gmail.com>
2023-12-21 01:48:18 +0400

    Update Product Card For Dark Mode and Normal

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function ProductCard({ product, onAddToCart }) {
   return (
-    <div className="flex flex-col justify-center items-center py-4 m-auto w-full lg:w-80 xl:w-80 border dark:bg-gray-900 dark:border-gray-800 border-transparent rounded-lg overflow-hidden shadow-lg hover:shadow-xl dark:hover:border-neutral-700 transition duration-500 hover:scale-110">
+    <div className="flex flex-col justify-center items-center py-4 m-auto w-full lg:w-80 hover:bg-neutral-200 dark:hover:bg-gray-900 xl:w-80 border dark:bg-gray-900 dark:border-gray-800 border-transparent rounded-lg overflow-hidden shadow-lg hover:shadow-xl dark:hover:border-neutral-700 transition duration-500 hover:scale-110">
       <div className="w-64 h-44">
         <Image
           src={product.image}
@@ -17,7 +17,7 @@ function ProductCard({ product, onAddToCart }) {
         <h3 className="text-lg font-bold">{product.name}</h3>
         <p className="text-gray-700 dark:text-gray-300">${product.price}</p>
         <button
-          className="mt-4 bg-yellow-500 hover:bg-yellow-600 dark:text-black text-black font-bold py-2 px-4 rounded-lg transition ease-in-out duration-500"
+          className="mt-4 bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded-lg transition ease-in-out duration-500"
           onClick={() => onAddToCart(product)}
         >
           Add to Cart

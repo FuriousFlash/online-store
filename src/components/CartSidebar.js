4ad2f30506eef4d78b761b0762c34f3080ab3100
FuriousFlash <adityakaushik38@gmail.com>
2023-12-21 01:47:27 +0400

    Update Cart Sidebar

--- src/components/CartSidebar.js
+++ src/components/CartSidebar.js
@@ -1,21 +1,41 @@
+import CartProductCard from "@components/CartProductCard";
+import { AiOutlineClose } from "react-icons/ai";
+
 function CartSidebar({ cart, onRemoveFromCart, onClose }) {
   return (
-    <aside className="fixed inset-y-0 right-0 w-64 bg-white dark:bg-gray-800 p-4 shadow-lg">
-      <button onClick={onClose}>Close</button>
-      <h2 className="text-2xl font-bold mb-4">Your Cart</h2>
-      <ul>
+    <aside className="fixed inset-y-0 right-0 w-72 bg-white dark:bg-gray-800 p-4 shadow-lg">
+      <div className="flex flex-row justify-between items-center mb-4">
+        <div>
+          <h2 className="text-2xl font-bold">Your Cart</h2>
+        </div>
+        <button
+          onClick={onClose}
+          className="border rounded-full p-2 hover:bg-neutral-300 dark:bg-gray-800 dark:hover:bg-gray-900 dark:hover:border-neutral-400"
+        >
+          <AiOutlineClose size={20} />
+        </button>
+      </div>
+      <div>
         {cart.map((item) => (
-          <li key={item.SKU} className="flex justify-between items-center mb-2">
-            <span>
-              {item.name} ({item.quantity})
-            </span>
-            <span>${(item.price * item.quantity).toFixed(2)}</span>
-            <button onClick={() => onRemoveFromCart(item)}>Remove</button>
-          </li>
+          <CartProductCard
+            key={item.SKU}
+            item={item}
+            onRemoveFromCart={onRemoveFromCart}
+          />
         ))}
-      </ul>
+        {cart.length === 0 && (
+          <div className="text-lg font-bold">Add items to continue...</div>
+        )}
+      </div>
       <div className="mt-4">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button
+          className={`${
+            cart.length === 0
+              ? "bg-gray-600 dark:text-black text-white cursor-not-allowed"
+              : "bg-green-500 hover:bg-green-600 text-black"
+          } font-bold py-2 px-4 rounded-lg`}
+          disabled={cart.length === 0}
+        >
           Checkout
         </button>
       </div>

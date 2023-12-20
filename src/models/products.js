646307cb7c51294c9a6b598e9b6c42bedb5df25b
FuriousFlash <adityakaushik38@gmail.com>
2023-12-21 00:47:10 +0400

    Adding images to products

--- src/models/products.js
+++ src/models/products.js
@@ -1,8 +1,8 @@
 const products = [
-  { SKU: "ipd", name: "Super iPad", price: 549.99 },
-  { SKU: "mbp", name: "MacBook Pro", price: 1399.99 },
-  { SKU: "atv", name: "Apple TV", price: 109.5 },
-  { SKU: "vga", name: "VGA adapter", price: 30.0 },
+  { SKU: "ipd", name: "Super iPad", price: 549.99, image: "/images/ipd.jpeg" },
+  { SKU: "mbp", name: "MacBook Pro", price: 1399.99, image: "/images/mbp.jpg" },
+  { SKU: "atv", name: "Apple TV", price: 109.5, image: "/images/atv.jpg" },
+  { SKU: "vga", name: "VGA adapter", price: 30.0, image: "/images/vga.jpeg" },
 ];
 
 export default products;

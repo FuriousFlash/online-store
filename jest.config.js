28c4f0f329156341388db8f5646fbe7d4a2c8d52
FuriousFlash <adityakaushik38@gmail.com>
2023-12-20 18:11:15 +0400

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   testEnvironment: "jest-environment-jsdom",
+  testMatch: ["<rootDir>/tests/**/*.test.js"],
 };

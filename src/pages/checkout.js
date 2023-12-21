077e0dccb498831af139dda1ce41e8dbb7544c55
FuriousFlash <adityakaushik38@gmail.com>
2023-12-21 05:56:33 +0400

    Refactoring checkout.js

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,57 +2,49 @@ import { useState, useEffect } from "react";
 import CartProductCard from "@components/CartProductCard";
 import useFetchPriceDetails from "@hooks/useFetchPriceDetails";
 import { useCart } from "@context/CartContext";
+import { useRouter } from "next/router";
+
+function CheckoutSummary({ priceBreakdown, totalPrice }) {
+  return (
+    <div className="flex flex-col gap-4">
+      {priceBreakdown.map((item, index) => (
+        <div key={index}>
+          <p>
+            {item.name} (x{item.quantity}) - Original: $
+            {(item.originalPrice * item.quantity).toFixed(2)}
+          </p>
+          {item.discountApplied > 0 && (
+            <p>
+              Discount: {item.discountDescription} -$
+              {item.discountApplied.toFixed(2)}
+            </p>
+          )}
+          <p>Final Price: ${item.finalPrice.toFixed(2)}</p>
+        </div>
+      ))}
+      <p className="font-bold">Total Price: ${totalPrice.toFixed(2)}</p>
+    </div>
+  );
+}
 
 function Checkout() {
-  const { cart, setCart, handleRemoveFromCart } = useCart();
-  const [orderPlaced, setOrderPlaced] = useState(false);
-
+  const { cart, handleRemoveFromCart } = useCart();
   const { fetchPriceDetails, totalPrice, priceBreakdown, error } =
     useFetchPriceDetails();
+  const [orderPlaced, setOrderPlaced] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     fetchPriceDetails(cart);
   }, [cart, fetchPriceDetails]);
+
   const handleCompletePurchase = () => {
     setOrderPlaced(true);
   };
 
   if (orderPlaced) {
-    return (
-      <div className="dark:bg-gray-800 flex flex-col mx-auto my-auto h-screen justify-center items-center p-4">
-        <h1 className="text-3xl font-bold mb-8">Checkout Successful</h1>
-        <p className="mb-4">
-          Thank you for your purchase! Here's a summary of your order:
-        </p>
-        <div className="flex flex-col justify-center items-start">
-          <div>
-            {priceBreakdown && (
-              <div>
-                {priceBreakdown.map((item, index) => (
-                  <div key={index}>
-                    <p>
-                      {item.name} (x{item.quantity}) - Final Price: $
-                      {item.finalPrice.toFixed(2)}
-                    </p>
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-        </div>
-
-        <p className="font-bold mt-4">Total Price: ${totalPrice.toFixed(2)}</p>
-        <button
-          className="text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded mt-4"
-          onClick={() => {
-            window.location.href = "/";
-            setCart([]);
-          }}
-        >
-          Continue Shopping
-        </button>
-      </div>
-    );
+    router.push("/success");
+    return null;
   }
 
   return (
@@ -75,29 +67,16 @@ function Checkout() {
           ))}
         </div>
         <div>
-          <h2 className="text-2xl font-bold mb-4">Order Summary</h2>
+          <h2 className="font-bold text-2xl mb-4">Order Summary</h2>
           <div className="p-4 border rounded">
-            {priceBreakdown && (
-              <div className="flex flex-col gap-4">
-                {priceBreakdown.map((item, index) => (
-                  <div key={index}>
-                    <p>
-                      {item.name} (x{item.quantity}) - Original: $
-                      {(item.originalPrice * item.quantity).toFixed(2)}
-                    </p>
-                    {item.discountApplied > 0 && (
-                      <p>
-                        Discount: {item.discountDescription} -$
-                        {item.discountApplied.toFixed(2)}
-                      </p>
-                    )}
-                    <p>Final Price: ${item.finalPrice.toFixed(2)}</p>
-                  </div>
-                ))}
-                <p className="font-bold">
-                  Total Price: ${totalPrice.toFixed(2)}
-                </p>
-              </div>
+            {error && <p className="error">{error.message}</p>}
+            {priceBreakdown ? (
+              <CheckoutSummary
+                priceBreakdown={priceBreakdown}
+                totalPrice={totalPrice}
+              />
+            ) : (
+              <p>Loading pricing details...</p>
             )}
             <button
               onClick={handleCompletePurchase}

e59f427d31708f4e40e679772bb26e2acab9882e
FuriousFlash <adityakaushik38@gmail.com>
2023-12-21 05:30:55 +0400

    Add cart provider to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
 import "@styles/globals.css";
+import { CartProvider } from "src/context/CartContext";
 
 function OnlineStore({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
+  );
 }
 
 export default OnlineStore;

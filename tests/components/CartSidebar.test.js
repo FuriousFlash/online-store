7f04388869b6df42ded7661f9361034a5a837338
FuriousFlash <adityakaushik38@gmail.com>
2023-12-21 08:53:58 +0400

    Update sidebar test

--- tests/components/CartSidebar.test.js
+++ tests/components/CartSidebar.test.js
@@ -1,33 +1,52 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import CartSidebar from "@components/CartSidebar";
 
+const mockCart = [
+  {
+    SKU: "ipd",
+    name: "Super iPad",
+    price: 549.99,
+    quantity: 2,
+    image: "/images/ipd.jpeg",
+  },
+  {
+    SKU: "mbp",
+    name: "MacBook Pro",
+    price: 1399.99,
+    quantity: 1,
+    image: "/images/mbp.jpg",
+  },
+];
+const mockHandleAddToCart = jest.fn();
+jest.mock("@context/CartContext", () => ({
+  useCart: () => ({
+    cart: mockCart,
+    setCart: jest.fn(),
+    handleAddToCart: mockHandleAddToCart,
+    handleRemoveFromCart: jest.fn(),
+    isCartInitialised: true,
+  }),
+}));
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "",
+      pathname: "",
+      query: "",
+      asPath: "",
+    };
+  },
+}));
+
 describe("CartSidebar", () => {
-  const mockCart = [
-    {
-      SKU: "ipd",
-      name: "Super iPad",
-      price: 549.99,
-      quantity: 2,
-      image: "/images/ipd.jpeg",
-    },
-    {
-      SKU: "mbp",
-      name: "MacBook Pro",
-      price: 1399.99,
-      quantity: 1,
-      image: "/images/mbp.jpg",
-    },
-  ];
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
   it("renders cart items, close button, and checkout button", () => {
     const mockClose = jest.fn();
-    render(
-      <CartSidebar
-        cart={mockCart}
-        onRemoveFromCart={() => {}}
-        onClose={mockClose}
-      />
-    );
+    render(<CartSidebar onClose={mockClose} />);
 
     expect(screen.getByText("Your Cart")).toBeInTheDocument();
     expect(screen.getAllByRole("button")).toHaveLength(mockCart.length + 2);
